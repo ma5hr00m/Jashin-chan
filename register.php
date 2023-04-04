c365bcafdbbe7fb268b722caa135ca86c8d46474
ma5hr00m <3411281455@qq.com>
2023-04-04 14:22:52 +0800

    add register page css

--- register.php
+++ register.php
@@ -1,26 +1,26 @@
 <?php
 include 'mysql.php';
 
+$notice = "Please input your username and password";
+
 if(isset($_POST['register'])) {
     
     $username = $_POST['username'];
     $password = $_POST['password'];
     
-    // 检查用户名是否存在
     $sql = "SELECT * FROM usrs WHERE username='$username'";
     $result = $conn->query($sql);
     
     if ($result->num_rows > 0) {
-        echo "The username is already taken!";
+        $notice = "The username is already taken!";
     } else {
-        // 注册信息插入数据库
         $sql = "INSERT INTO usrs (username, password) VALUES ('$username', '$password')";
         
         if ($conn->query($sql) === TRUE) {
             header("Location: login.php");
             exit;
         } else {
-            echo "Register failed";
+            $notice =  "Register failed";
         }
     }
 }
@@ -29,21 +29,26 @@ if(isset($_POST['register'])) {
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Register</title>
-	<meta charset="utf-8">
-</head>
-<body>
-	<h1>Register Page</h1>
-    <button onclick="location.href = 'login.php'" >Back to Login</button>
-	<form method="post" action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>'>
-		<label>Username</label>
-		<input type="text" name="username" required>
-        <br>
-		<label>Password</label>
-		<input type="password" name="password" required>
-        <br>
-		<input type="submit" name="register" value="Register">
-	</form>
-</body>
+    <head>
+        <title>Register Page</title>
+        <link rel="stylesheet" href="./src/css/login.css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <main>
+            <h1>Wirror</h1>
+            <h2>Create an account</h2>
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                <label id="username-label" class="label">Username</label>
+                <input id="username" class="input" type="text" name="username" required>
+                <label id="password-label" class="label">Password</label>
+                <input id="password" class="input" type="password" name="password" required>
+                <span id="notice" class="tip"><?php echo $notice;?></span>
+                <input id="login" type=submit name="register" value="Register">
+            </form>
+            <span id="register-tip">
+                Already have an account? <a id="link" href="login.php">Sign in</a>
+            </span>
+        </main>
+    </body>
 </html>
